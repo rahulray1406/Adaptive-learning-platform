466dbf2969887361871335994bd262d7e319d8ed
rahul ray <rahul@navigus.in>
2022-01-22 00:06:26 +0530

    fix(env): Fixed oath redirection issue.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 require("./database");
-require("./passportSetup")
-const path = require("path")
-const passport = require('passport');
-var MemoryStore = require('memorystore')(session)
+require("./passportSetup");
+const path = require("path");
+const passport = require("passport");
+var MemoryStore = require("memorystore")(session);
 const expHbs = require("express-handlebars");
 const helper = require("handlebars-helpers")();
 const teacherRoute = require("./routes/teacherRoute");
@@ -13,140 +13,139 @@ const studentRoute = require("./routes/studentRoute");
 const teacher = require("./models/teacher");
 const student = require("./models/student");
 const course = require("./models/course");
-const reports = require('./models/reports')
+const reports = require("./models/reports");
 
 const router = require("./routes/teacherRoute");
 const app = express();
 
-
-
-app.use(session({
+app.use(
+  session({
     cookie: { maxAge: 86400000 },
     store: new MemoryStore({
-        checkPeriod: 86400000 // prune expired entries every 24h
+      checkPeriod: 86400000, // prune expired entries every 24h
     }),
     secret: "1234asdf",
     resave: false,
     saveUninitialized: false,
-    maxAge: 3600000
-}
-))
+    maxAge: 3600000,
+  })
+);
 app.use(passport.initialize());
-app.use(passport.session())
+app.use(passport.session());
 app.use(teacherRoute);
 app.use(studentRoute);
 var hbs = expHbs.create({
-    extname: "hbs",
-    defaultLayout: "main",
-    layoutsDir: path.join(__dirname, "views/layout"),
-    helpers: helper,
-    partialsDir: path.join(__dirname, "views/partials"),
-})
+  extname: "hbs",
+  defaultLayout: "main",
+  layoutsDir: path.join(__dirname, "views/layout"),
+  helpers: helper,
+  partialsDir: path.join(__dirname, "views/partials"),
+});
 app.engine("hbs", hbs.engine);
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "/public")));
-app.get('/', (req, res) => {
-    res.render("landing");
-})
-
-app.get('/teacherLogin', (req, res) => {
-    req.session.studentTryLogin = false;
-    req.session.teacherTryLogin = true;
-    console.log(req.session)
-    res.redirect('/auth/google')
+app.get("/", (req, res) => {
+  res.render("landing");
 });
 
-app.get('/studentLogin', (req, res) => {
-    req.session.studentTryLogin = true;
-    req.session.teacherTryLogin = false;
-    console.log(req.session)
-    res.redirect('/auth/google')
+app.get("/teacherLogin", (req, res) => {
+  req.session.studentTryLogin = false;
+  req.session.teacherTryLogin = true;
+  console.log(req.session);
+  res.redirect("/auth/google");
 });
 
-app.get('/auth/google',
-    passport.authenticate('google', {
-        scope:
-            ['email', 'profile']
-    }
-    ));
+app.get("/studentLogin", (req, res) => {
+  req.session.studentTryLogin = true;
+  req.session.teacherTryLogin = false;
+  console.log(req.session);
+  res.redirect("/auth/google");
+});
 
-app.get('/auth/google/callback',
-    passport.authenticate('google', {
-        successRedirect: '/auth/google/success',
-        failureRedirect: '/auth/google/failure'
-    }));
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/auth/google/success",
+    failureRedirect: "/auth/google/failure",
+  })
+);
 
 app.get("/auth/google/success", (req, res) => {
-    console.log("Success")
-    console.log(req.session)
-    console.log(req.user)
-    let flag = false;
-    if(req.session.studentTryLogin)
-    {
-        console.log("Teacher not fou, find stuent ry")
-        student.findById(req.user)
-            .then(d => {
-                console.log(d);
-                if (!flag) {
-                    req.session.studentTryLogin = false;
-                    return res.redirect('/student/studentRedirect?id=' + d.studentID)
-                }
-            })
-            .catch(e => console.log(e))
-    }
-    else
-    {
-        teacher.findById(req.user)
-        .then(d => {
-            console.log("teacher found")
-            console.log(d);
-            // res.send(d);
-            if (d) {
-                flag = true;
-                req.session.studentTryLogin = false;
-                return res.redirect('/teacher/teacherRedirect?id=' + d.teacherID)
-            }
-            else {
-               
-            }
-
-        })
-        .catch(e => console.log(e))
-    }
-    
-})
-app.get('testRedirect', (req, res) => {
-    res.send("Redirect successfull")
-})
+  console.log("Success");
+  console.log(req.session);
+  console.log(req.user);
+  let flag = false;
+  if (req.session.studentTryLogin) {
+    console.log("Teacher not fou, find stuent ry");
+    student
+      .findById(req.user)
+      .then((d) => {
+        console.log(d);
+        if (!flag) {
+          req.session.studentTryLogin = false;
+          return res.redirect("/student/studentRedirect?id=" + d.studentID);
+        }
+      })
+      .catch((e) => console.log(e));
+  } else {
+    teacher
+      .findById(req.user)
+      .then((d) => {
+        console.log("teacher found");
+        console.log(d);
+        // res.send(d);
+        if (d) {
+          flag = true;
+          req.session.studentTryLogin = false;
+          return res.redirect("/teacher/teacherRedirect?id=" + d.teacherID);
+        } else {
+        }
+      })
+      .catch((e) => console.log(e));
+  }
+});
+app.get("testRedirect", (req, res) => {
+  res.send("Redirect successfull");
+});
 
 app.get("/auth/google/failure", (req, res) => {
-    console.log("Failed")
-    console.log(req.user)
-    console.log(req.session)
-    console.log(req.isAuthenticated())
-    res.send("login Failed XXXXX")
-})
+  console.log("Failed");
+  console.log(req.user);
+  console.log(req.session);
+  console.log(req.isAuthenticated());
+  res.send("login Failed XXXXX");
+});
 
-app.post('/ansSubmit', (req, res) => {
-    console.log(req.body);
-    console.log("----data of ansSubmit-----")
-    console.log(req.body)
-    const newReport = new reports({
-        courseID: req.body.courseID,
-        studentID: req.body.studentID,
-        totalMarks: req.body.totalMarks,
-        totalAttempt: req.body.totalAttempt,
-        marksAchived: req.body.marksAchived,
-        startTime: req.body.startTime,
-        endTime: req.body.endTime
-    });
-    newReport.save()
-        .then(d => {
-            console.log("-data form mongoose--")
-            console.log(d)
-            res.send(d);
-        })
-        .catch(e => console.log(e));
-})
-
-app.listen(80, (req, res) => console.log("server running at :: http://localhost:80"));
+app.post("/ansSubmit", (req, res) => {
+  console.log(req.body);
+  console.log("----data of ansSubmit-----");
+  console.log(req.body);
+  const newReport = new reports({
+    courseID: req.body.courseID,
+    studentID: req.body.studentID,
+    totalMarks: req.body.totalMarks,
+    totalAttempt: req.body.totalAttempt,
+    marksAchived: req.body.marksAchived,
+    startTime: req.body.startTime,
+    endTime: req.body.endTime,
+  });
+  newReport
+    .save()
+    .then((d) => {
+      console.log("-data form mongoose--");
+      console.log(d);
+      res.send(d);
+    })
+    .catch((e) => console.log(e));
+});
+const PORT = process.env.PORT;
+app.listen(PORT, (req, res) =>
+  console.log(`server running at :: http://localhost:${PORT}`)
+);
--- passportSetup.js
+++ passportSetup.js
@@ -1,111 +1,115 @@
-const student = require("./models/student")
-const teacher = require("./models/teacher")
-const passport = require('passport');
-const googleStrategy = require('passport-google-oauth2');
+const student = require("./models/student");
+const teacher = require("./models/teacher");
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth2");
 const { request } = require("express");
 
 passport.serializeUser(function (user, done) {
-    console.log("Serialize user")
-    console.log(user)
-    return done(null, user._id);
+  console.log("Serialize user");
+  console.log(user);
+  return done(null, user._id);
 });
 
 passport.deserializeUser(function (userId, done) {
-    console.log("Deserilize uid: ")
-    console.log(userId)
-    done(null, userId);
-    // return done(null, userId);
+  console.log("Deserilize uid: ");
+  console.log(userId);
+  done(null, userId);
+  // return done(null, userId);
 });
 
-passport.use(new googleStrategy({
-    clientID: "958559105804-01b7hs7eegahnoo84si2sfu4hmr95901.apps.googleusercontent.com",
-    clientSecret: "8QT0OSyOdhkjIJueZ2j4t9fT",
-    callbackURL: process.env.AUTH_URL,
-    passReqToCallback: true
-},
+passport.use(
+  new googleStrategy(
+    {
+      clientID:
+        "958559105804-01b7hs7eegahnoo84si2sfu4hmr95901.apps.googleusercontent.com",
+      clientSecret: "8QT0OSyOdhkjIJueZ2j4t9fT",
+      callbackURL: process.env.AUTH_URL,
+      passReqToCallback: true,
+    },
     async function (request, accessToken, refreshToken, profile, done) {
-        console.log(profile.given_name);
-        console.log(profile.family_name)
-        console.log(profile.email)
-        console.log(profile.picture)
-        console.log("Session data")
-        console.log(request.session)
-        if (request.session.teacherTryLogin) {
-            teacher.findOne({ email: profile.email })
-                .then(data => {
-                    if (data) {
+      console.log(profile.given_name);
+      console.log(profile.family_name);
+      console.log(profile.email);
+      console.log(profile.picture);
+      console.log("Session data");
+      console.log(request.session);
+      if (request.session.teacherTryLogin) {
+        teacher
+          .findOne({ email: profile.email })
+          .then((data) => {
+            if (data) {
+              return done(null, data);
+            } else {
+              let newreq;
+              teacher
+                .aggregate([
+                  { $group: { _id: null, maxid: { $max: "$teacherID" } } },
+                ])
+                .then((d) => {
+                  console.log(d[0]);
+                  newreq = d[0].maxid + 1;
+                  const nayTeacher = new teacher({
+                    teacherID: newreq,
+                    fname: profile.given_name,
+                    lname: profile.family_name,
+                    profilePicture: profile.picture,
+                    email: profile.email,
+                  });
+                  nayTeacher.save().then((ans) => {
+                    teacher.findOne({ email: profile.email }).then((data) => {
+                      console.log(data);
+                      return done(null, data);
+                    });
+                  });
+                });
+            }
+          })
+          .catch((err) => console.log(err));
+      } else {
+        console.log("Student login inti");
+        console.log(request.session.studentTryLogin);
+        student
+          .findOne({ email: profile.email })
+          .then((data) => {
+            if (data) {
+              console.log("Student login inti: STUDENT FOUND");
+              return done(null, data);
+            } else {
+              let newreq;
+              student
+                .aggregate([
+                  { $group: { _id: null, maxid: { $max: "$studentID" } } },
+                ])
+                .then((d) => {
+                  console.log(d[0]);
+                  newreq = d[0].maxid + 1;
+                  const naystudent = new student({
+                    studentID: newreq,
+                    fname: profile.given_name,
+                    lname: profile.family_name,
+                    profilePicture: profile.picture,
+                    email: profile.email,
+                    points: 1,
+                    expPoints: 1,
+                    teacherID: 9999,
+                  });
+                  naystudent
+                    .save()
+                    .then((ans) => {
+                      console.log("Student login inti:STUDNT SAVED");
+                      student.findOne({ email: profile.email }).then((data) => {
+                        console.log(data);
                         return done(null, data);
-                    }
-                    else {
-                        let newreq;
-                        teacher.aggregate([{ $group: { _id: null, maxid: { $max: "$teacherID" } } }])
-                            .then(d => {
-                                console.log(d[0])
-                                newreq = d[0].maxid + 1;
-                                const nayTeacher = new teacher({
-                                    teacherID: newreq,
-                                    fname: profile.given_name,
-                                    lname: profile.family_name,
-                                    profilePicture: profile.picture,
-                                    email: profile.email
-                                })
-                                nayTeacher.save()
-                                    .then(ans => {
-                                        teacher.findOne({ email: profile.email })
-                                            .then(data => {
-                                                console.log(data)
-                                                return done(null, data);
-                                            })
-                                    })
-                            })
-
-                    }
-                })
-                .catch(err => console.log(err));
-        }
-        else {
-            console.log("Student login inti")
-            console.log(request.session.studentTryLogin)
-            student.findOne({ email: profile.email })
-                .then(data => {
-                    if (data) {
-                        console.log("Student login inti: STUDENT FOUND")
-                        return done(null, data);
-                    }
-                    else {
-                        let newreq;
-                        student.aggregate([{ $group: { _id: null, maxid: { $max: "$studentID" } } }])
-                            .then(d => {
-                                console.log(d[0])
-                                newreq = d[0].maxid + 1;
-                                const naystudent = new student({
-                                    studentID: newreq,
-                                    fname: profile.given_name,
-                                    lname: profile.family_name,
-                                    profilePicture: profile.picture,
-                                    email: profile.email,
-                                    points: 1,
-                                    expPoints: 1,
-                                    teacherID: 9999
-                                })
-                                naystudent.save()
-                                    .then(ans => {
-                                        console.log("Student login inti:STUDNT SAVED")
-                                        student.findOne({ email: profile.email })
-                                            .then(data => {
-                                                console.log(data)
-                                                return done(null, data);
-                                            })
-                                    })
-                                    .catch(e => {
-                                        console.log(e);
-                                    })
-                            })
-
-                    }
-                })
-                .catch(err => console.log(err));
-        }
-
+                      });
+                    })
+                    .catch((e) => {
+                      console.log(e);
+                    });
+                });
+            }
+          })
+          .catch((err) => console.log(err));
+      }
     }
-));
+  )
+);
